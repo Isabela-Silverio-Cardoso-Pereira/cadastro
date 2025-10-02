e103c44aa59a2f2e27af7c2fb4eda2550e334954
Isabela Silvério <145703412+Isabela-Silverio-Cardoso-Pereira@users.noreply.github.com>
2025-10-02 19:22:27 -0300

    Update and rename ex2_1.js to cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,13 @@
+const frm = document.querySelector("form")
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = document.querySelector("#inNome").value
+
+    const numero = document.querySelector("#innumero").value
+    const email = document.querySelector("#inemail").value
+    const senha = document.querySelector("#insenha").value
+
+})
